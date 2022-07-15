3d2660dea219411f212c19793e40787d331eb5d8
AM-student <93679555+AM-student@users.noreply.github.com>
2022-07-15 23:56:04 +0200

    syntax changes

--- carapp/src/app/models/vwgolf.ts
+++ carapp/src/app/models/vwgolf.ts
@@ -1,10 +1,10 @@
 export interface VWGolf {
-  vwg_id: string,
-  vwg_image: string,
-  vwg_name: string,
-  vwg_type: string
-  vwg_year: number
-  vwg_vin: number
-  vwg_price: number
-  description_vwg: string
+  vwg_id: string;
+  vwg_image: string;
+  vwg_name: string;
+  vwg_type: string;
+  vwg_year: number;
+  vwg_vin: number;
+  vwg_price: number;
+  description_vwg: string;
   }
